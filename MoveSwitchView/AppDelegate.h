c1486c585eb5099133e88a6e5a7a9d10e7b61278
阳永辉 <hopeyyh@qq.com>
2016-06-22 10:58:42 +0800

    Initial Commit

---
+++ MoveSwitchView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoveSwitchView
+//
+//  Created by 阳永辉 on 16/6/22.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoveSwitchView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoveSwitchView
+//
+//  Created by 阳永辉 on 16/6/22.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
